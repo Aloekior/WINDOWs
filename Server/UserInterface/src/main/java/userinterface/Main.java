3fe807f1ea3802ffa79ba58b673e23fb9974852b
Aloekior <purpledesert@posteo.nz>
2022-07-28 23:02:30 +0200

    simplified user commands

--- Server/UserInterface/src/main/java/userinterface/Main.java
+++ Server/UserInterface/src/main/java/userinterface/Main.java
@@ -3,6 +3,7 @@ package userinterface;
 import userinterface.objects.DatabaseConnection;
 import userinterface.objects.User;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -17,16 +18,16 @@ public class Main {
             while (!quitApplication) {
                 System.out.print("\nPlease enter a command: ");
                 String command = scanner.nextLine();
-                switch (command) {
-                    case "addSensor" -> database.addSensor();
-                    case "deactivateSensor" -> database.deactivateSensor();
-                    case "changeSensorRoom" -> database.changeSensorLocation(false);
-                    case "changeSensorWindow" -> database.changeSensorLocation(true);
-                    case "printStates" -> database.printCurrentStatesPrepareQuery(false);
-                    case "printRoom" -> database.printCurrentStatesPrepareQuery(true);
-                    case "printHistory" -> database.printHistory();
-                    case "createUser" -> database.userOption(true);
-                    case "deleteUser" -> database.userOption(false);
+                switch (command.toLowerCase()) {
+                    case "add sensor" -> database.addSensor();
+                    case "deactivate sensor" -> database.deactivateSensor();
+                    case "change sensor room" -> database.changeSensorLocation(false);
+                    case "change sensor window" -> database.changeSensorLocation(true);
+                    case "states" -> database.printCurrentStatesPrepareQuery(false);
+                    case "room" -> database.printCurrentStatesPrepareQuery(true);
+                    case "history" -> database.printHistory();
+                    case "create user" -> database.userOption(true);
+                    case "delete user" -> database.userOption(false);
                     case "exit" -> quitApplication = true;
                     default -> {
                         System.out.println("Unknown command '" + command + "'\n");
@@ -53,15 +54,15 @@ public class Main {
     public static void printHelp() {
         System.out.println("""
                         Available Commands:
-                        addSensor           initiates procedure to add a new sensor to the system
-                        deactivateSensor    remove sensor from the system (sensor will be set inactive)
-                        changeSensorRoom    allows to change a sensors assigned room name
-                        changeSensorWindow  like changeRoom, just for window within a room
-                        printStates         prints all last reported sensor states
-                        printRoom           prints last reported sensor states assigned to the entered room only
-                        printHistory        prints 50 most recent history entries
-                        createUser          (ADMIN ONLY) Create new read-sensors-only database user
-                        deleteUser          (ADMIN ONLY) Delete read-sensors-only database user
+                        add                 (sensor) initiates procedure to add a new sensor to the system
+                        deactivate          (sensor) remove sensor from the system (will be set inactive)
+                        change room         allows to change a sensors assigned room name
+                        change window       like 'change room', just for window within a room
+                        states              prints all last reported sensor states
+                        room                prints last reported sensor states assigned to the entered room only
+                        history             prints 50 most recent history entries
+                        create user         (ADMIN ONLY) Create new read-sensors-only database user
+                        delete user         (ADMIN ONLY) Delete read-sensors-only database user
                         exit                quit application
                         """);
     }
