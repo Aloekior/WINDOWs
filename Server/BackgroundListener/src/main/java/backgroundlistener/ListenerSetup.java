4ab72ba0030a415658f9c2053be36ccfc2852021
Aloekior <purpledesert@posteo.nz>
2022-08-01 21:51:35 +0200

    fixed missing line ends when writing to file

--- Server/BackgroundListener/src/main/java/backgroundlistener/ListenerSetup.java
+++ Server/BackgroundListener/src/main/java/backgroundlistener/ListenerSetup.java
@@ -33,8 +33,8 @@ public class ListenerSetup {
         String password = scanner.nextLine();
         
         try (FileWriter configWrite = new FileWriter(filename)){
-            configWrite.write(url);
-            configWrite.write(username);
+            configWrite.write(url + "\n");
+            configWrite.write(username + "\n");
             configWrite.write(password);
             System.out.println("Successfully written to file");
         } catch (IOException e) {
